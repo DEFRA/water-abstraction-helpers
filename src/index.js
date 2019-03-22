2a6769e8f499128e4b5cae9d37a2c6093e6dc439
James Carmichael <james@siteclick.co.uk>
2019-03-22 16:31:21 +0000

    Adds service request API from UI project

--- src/index.js
+++ src/index.js
@@ -2,3 +2,5 @@ exports.logger = require('./logger');
 exports.nald = require('./nald');
 exports.regions = require('./regions');
 exports.returns = require('./returns');
+exports.http = require('./http');
+exports.serviceRequest = require('./service-request');
