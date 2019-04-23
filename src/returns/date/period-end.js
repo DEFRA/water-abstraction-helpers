8a239e218e6a40555ebd5444f4989a68300e21ae
James Carmichael <james@siteclick.co.uk>
2019-04-23 16:53:06 +0100

    Feat/return period end (#35)
    * Adds a function similar getPeriodEnd, which gets the return cycle end date for a given date
    
    * Bump version and changelog

---
+++ src/returns/date/period-end.js
@@ -0,0 +1,21 @@
+const moment = require('moment');
+const { getPeriodStart } = require('./period-start');
+
+/**
+ * Given a particular date and a isSummer flag, calculates the end date
+ * of the returns cycle that date lies in
+ * @param {String} date - comparison date
+ * @param {Boolean} isSummer - whether summer return
+ * @return {String} YYYY-MM-DD period end date
+ */
+const getPeriodEnd = (date, isSummer) => {
+  const startDate = getPeriodStart(date, isSummer);
+  return moment(startDate)
+    .add(1, 'year')
+    .subtract(1, 'day')
+    .format('YYYY-MM-DD');
+};
+
+module.exports = {
+  getPeriodEnd
+};
---
+++ test/returns/date/period-end.js
@@ -0,0 +1,38 @@
+const Lab = require('lab');
+
+const { experiment, test } = exports.lab = Lab.script();
+const { expect } = require('code');
+
+const { returns: { date: { getPeriodEnd } } } = require('../../../src');
+
+experiment('getPeriodEnd - winter', () => {
+  // ---------- getPeriodEnd - winter
+  test('getPeriodEnd for winter year return in same year', async () => {
+    expect(getPeriodEnd('2018-05-01')).to.equal('2019-03-31');
+  });
+  test('getPeriodEnd for winter year return in different year', async () => {
+    expect(getPeriodEnd('2018-02-01')).to.equal('2018-03-31');
+  });
+  test('getPeriodEnd for winter year return on period start date', async () => {
+    expect(getPeriodEnd('2018-04-01')).to.equal('2019-03-31');
+  });
+  test('getPeriodEnd for winter year return on period end date', async () => {
+    expect(getPeriodEnd('2018-03-31')).to.equal('2018-03-31');
+  });
+});
+
+experiment('getPeriodEnd - summer', () => {
+// ---------- getPeriodEnd - summer
+  test('getPeriodEnd for summer year return in same year', async () => {
+    expect(getPeriodEnd('2018-12-01', true)).to.equal('2019-10-31');
+  });
+  test('getPeriodEnd for summer year return in different year', async () => {
+    expect(getPeriodEnd('2018-02-01', true)).to.equal('2018-10-31');
+  });
+  test('getPeriodEnd for summer year return on period start date', async () => {
+    expect(getPeriodEnd('2018-11-01', true)).to.equal('2019-10-31');
+  });
+  test('getPeriodEnd for summer year return on period end date', async () => {
+    expect(getPeriodEnd('2018-10-31', true)).to.equal('2018-10-31');
+  });
+});
