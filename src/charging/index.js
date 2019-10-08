32ea4c00529f1ad16e37f0e75f4555d5ee4a3d75
James Carmichael <james@siteclick.co.uk>
2019-10-08 14:11:24 +0100

    Add mergeHistory function which merges arrays of objects where date ranges are adjacent

--- src/charging/index.js
+++ src/charging/index.js
@@ -1,7 +1,9 @@
 const dateRangeSplitter = require('./date-range-splitter');
 const billableDays = require('./billable-days');
+const mergeHistory = require('./merge-history');
 
 module.exports = {
   ...dateRangeSplitter,
-  ...billableDays
+  ...billableDays,
+  ...mergeHistory
 };
