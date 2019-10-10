d7f0615162b6326cade98c534f4e017aab2fa4af
Dana Gajewska <danusia.x@outlook.com>
2019-10-10 10:24:38 +0100

    Move isDateWithinAbstractionPeriod function from UI to Helpers

--- src/returns/date/index.js
+++ src/returns/date/index.js
@@ -1,9 +1,11 @@
 const { createReturnCycles } = require('./returns-cycles');
 const { getPeriodStart } = require('./period-start');
 const { getPeriodEnd } = require('./period-end');
+const { isDateWithinAbstractionPeriod } = require('./date-in-abstraction-period');
 
 module.exports = {
   createReturnCycles,
   getPeriodStart,
-  getPeriodEnd
+  getPeriodEnd,
+  isDateWithinAbstractionPeriod
 };
