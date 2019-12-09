6109bee04796eb54b5d8cb8957ddbbc55ff88f67
James Carmichael <james@siteclick.co.uk>
2019-12-09 15:40:59 +0000

    Add validation to isDateWithinAbstractionPeriod function, and convert strings to integers in supplied abstraction period

--- test/returns/date/date-in-abstracton-period.js
+++ test/returns/date/date-in-abstracton-period.js
@@ -13,6 +13,13 @@ const sameYear = {
   periodEndMonth: 12
 };
 
+const sameYearStrings = {
+  periodStartDay: '5',
+  periodStartMonth: '3',
+  periodEndDay: '25',
+  periodEndMonth: '12'
+};
+
 const differentYear = {
   periodStartDay: 1,
   periodStartMonth: 10,
@@ -20,11 +27,32 @@ const differentYear = {
   periodEndMonth: 6
 };
 
+const differentYearStrings = {
+  periodStartDay: '1',
+  periodStartMonth: '10',
+  periodEndDay: '8',
+  periodEndMonth: '6'
+};
+
 const allYear = {
   periodStartDay: 1,
   periodStartMonth: 1,
   periodEndDay: 31,
   periodEndMonth: 12
+};
+
+const allYearStrings = {
+  periodStartDay: '1',
+  periodStartMonth: '1',
+  periodEndDay: '31',
+  periodEndMonth: '12'
+};
+
+const invalid = {
+  periodStartDay: 1,
+  periodStartMonth: 1,
+  periodEndDay: 31,
+  periodEndMonth: 13
 };
 
 experiment('Test isDateWithinAbstractionPeriod', () => {
@@ -37,6 +65,15 @@ experiment('Test isDateWithinAbstractionPeriod', () => {
     expect(isDateWithinAbstractionPeriod('2018-12-31', sameYear)).to.equal(false);
   });
 
+  test('Period start/end in same year - abstraction period as strings', async () => {
+    expect(isDateWithinAbstractionPeriod('2018-01-01', sameYearStrings)).to.equal(false);
+    expect(isDateWithinAbstractionPeriod('2018-03-04', sameYearStrings)).to.equal(false);
+    expect(isDateWithinAbstractionPeriod('2018-03-05', sameYearStrings)).to.equal(true);
+    expect(isDateWithinAbstractionPeriod('2018-12-25', sameYearStrings)).to.equal(true);
+    expect(isDateWithinAbstractionPeriod('2018-12-26', sameYearStrings)).to.equal(false);
+    expect(isDateWithinAbstractionPeriod('2018-12-31', sameYearStrings)).to.equal(false);
+  });
+
   test('Period start/end in different year', async () => {
     expect(isDateWithinAbstractionPeriod('2018-09-30', differentYear)).to.equal(false);
     expect(isDateWithinAbstractionPeriod('2018-10-01', differentYear)).to.equal(true);
@@ -46,10 +83,31 @@ experiment('Test isDateWithinAbstractionPeriod', () => {
     expect(isDateWithinAbstractionPeriod('2019-06-09', differentYear)).to.equal(false);
   });
 
+  test('Period start/end in different year - abstraction period as strings', async () => {
+    expect(isDateWithinAbstractionPeriod('2018-09-30', differentYearStrings)).to.equal(false);
+    expect(isDateWithinAbstractionPeriod('2018-10-01', differentYearStrings)).to.equal(true);
+    expect(isDateWithinAbstractionPeriod('2018-12-31', differentYearStrings)).to.equal(true);
+    expect(isDateWithinAbstractionPeriod('2019-01-01', differentYearStrings)).to.equal(true);
+    expect(isDateWithinAbstractionPeriod('2019-06-08', differentYearStrings)).to.equal(true);
+    expect(isDateWithinAbstractionPeriod('2019-06-09', differentYearStrings)).to.equal(false);
+  });
+
   test('Period all year', async () => {
     expect(isDateWithinAbstractionPeriod('2017-12-31', allYear)).to.equal(true);
     expect(isDateWithinAbstractionPeriod('2018-01-01', allYear)).to.equal(true);
     expect(isDateWithinAbstractionPeriod('2018-12-31', allYear)).to.equal(true);
     expect(isDateWithinAbstractionPeriod('2019-01-01', allYear)).to.equal(true);
+  });
+
+  test('Period all year - abstraction period as strings', async () => {
+    expect(isDateWithinAbstractionPeriod('2017-12-31', allYearStrings)).to.equal(true);
+    expect(isDateWithinAbstractionPeriod('2018-01-01', allYearStrings)).to.equal(true);
+    expect(isDateWithinAbstractionPeriod('2018-12-31', allYearStrings)).to.equal(true);
+    expect(isDateWithinAbstractionPeriod('2019-01-01', allYearStrings)).to.equal(true);
+  });
+
+  test('Throws an error when abstraction period is invalid', async () => {
+    const func = () => isDateWithinAbstractionPeriod('2019-01-01', invalid);
+    expect(func).to.throw();
   });
 });
