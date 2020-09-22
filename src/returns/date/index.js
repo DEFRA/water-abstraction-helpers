fa455b496145f0119f74e5e6afd32f5bf7a84222
Stephan Hough <43169948+Stephan-Hough@users.noreply.github.com>
2020-09-22 12:22:51 +0100

    feat/2803 -- new helper to get the abstraction period season

--- src/returns/date/index.js
+++ src/returns/date/index.js
@@ -2,10 +2,13 @@ const { createReturnCycles } = require('./returns-cycles');
 const { getPeriodStart } = require('./period-start');
 const { getPeriodEnd } = require('./period-end');
 const { isDateWithinAbstractionPeriod } = require('./date-in-abstraction-period');
+const { getAbstractionPeriodSeason, isWithinAbstractionPeriod } = require('./season-in-abstraction-period');
 
 module.exports = {
   createReturnCycles,
   getPeriodStart,
   getPeriodEnd,
-  isDateWithinAbstractionPeriod
+  isDateWithinAbstractionPeriod,
+  getAbstractionPeriodSeason,
+  isWithinAbstractionPeriod
 };
