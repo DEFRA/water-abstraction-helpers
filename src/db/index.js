87ef8d28da9e50f40729f8b1b3915d75a09028db
Jonathan Goulding <jonathangoulding21@gmail.com>
2022-04-19 11:49:30 +0100

    chore: update helpers version

--- src/db/index.js
+++ src/db/index.js
@@ -1,16 +1,33 @@
 'use strict';
 const pg = require('pg');
 
+/**
+ * Formats a log message if pool low on connections
+ * @param {Object} pool - pg pool instance
+ * @return {String} - the message to log
+ */
 const formatLogMessage = pool => {
   const { totalCount, idleCount, waitingCount } = pool;
   return `Pool low on connections::Total:${totalCount},Idle:${idleCount},Waiting:${waitingCount}`;
 };
 
+/**
+ * Determines whether to log message if pool low on connections
+ * @param {Object} config - pg options
+ * @param {Object} pool - pg pool instance
+ * @return {Boolean} - whether to log message
+ */
 const isLogMessage = (config, pool) => {
   const { totalCount, idleCount, waitingCount } = pool;
   return totalCount === config.max && idleCount === 0 && waitingCount > 0;
 };
 
+/**
+ * Registers event listeners on the PG pool instance
+ * @param {Pool} pool
+ * @param {Object} config
+ * @param {Logger} logger
+ */
 const registerEventListeners = (pool, config, logger) => {
   pool.on('acquire', () => {
     if (isLogMessage(config, pool)) {
@@ -22,12 +39,32 @@ const registerEventListeners = (pool, config, logger) => {
   });
 };
 
+/**
+ * Creates a Postgres pool instance
+ * @param {Object} config - pg options
+ * @param {Object} postgres pool instance
+ */
 const createPool = (config, logger) => {
   const pool = new pg.Pool(config);
   registerEventListeners(pool, config, logger);
   return pool;
 };
 
+/**
+ * Maps a query and params written for use with pg.pool
+ * so that it is compatible with knex.raw.
+ *
+ * With pg.pool the position of the parameter in the 1-based array
+ * is used, prefixed with a $ symbol, e.g.:
+ * `select * from table where name=$1`, ['bob']
+ *
+ * With knex, named parameters and a hash is generallly used, e.g.:
+ * `select * from table where name=:name`, { name : 'bob' }
+ *
+ * @param {String} sqlQuery - bound params are specified as $1, $2 etc.
+ * @param {Array} [params] - array params
+ * @return {Object} [query, params]
+ */
 const mapQueryToKnex = (query, params = []) => {
   if (params.length === 0) {
     return [query];
