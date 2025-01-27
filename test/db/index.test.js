05a0367ed5d4ca58cc713b855b170d6ecae7f217
Alan Cruikshanks <alan.cruikshanks@gmail.com>
2025-01-27 19:57:15 +0000

    Remove redundant tests

--- test/db/index.test.js
+++ test/db/index.test.js
@@ -43,33 +43,6 @@ experiment('db/index.js', () => {
       expect(pool.constructor.name).to.equal('BoundPool')
     })
 
-    experiment('when a client is acquired', () => {
-      experiment('if pool is waiting for an available client', () => {
-        beforeEach(async () => {
-          sandbox.stub(pool, 'waitingCount').get(() => 1)
-          sandbox.stub(pool, 'totalCount').get(() => 8)
-          pool.emit('acquire', client)
-        })
-
-        test('an info message is logged', async () => {
-          const [msg] = logger.info.lastCall.args
-          expect(msg).to.equal('Pool low on connections::Total:8,Idle:0,Waiting:1')
-        })
-      })
-
-      experiment('if pool is not waiting for an available client', () => {
-        beforeEach(async () => {
-          sandbox.stub(pool, 'waitingCount').get(() => 0)
-          sandbox.stub(pool, 'totalCount').get(() => 8)
-          pool.emit('acquire', client)
-        })
-
-        test('no messages are logged', async () => {
-          expect(logger.info.called).to.be.false()
-        })
-      })
-    })
-
     experiment('when a client errors', () => {
       const err = new Error('some terrible problem')
 
